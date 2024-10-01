e61bb7c468268f786186012d22b26fb6ca389c9e
TheRealKuba <jkokes22@gmail.com>
2024-10-01 08:09:31 +0200

    první můj apdejt juchůůů

---
+++ git_config.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+git config --global user.name "ondrej_masny"   
+git config --global user.email "icelyy@seznam.cz"
+echo "Přihlášení uživatele proběhlo v pořádku"
+ls 
+# cd eshop_biocolors
+# php artisan serve
+# https://fonts.bunny.net
+# https://www.fontshare.com/fonts/gambetta
